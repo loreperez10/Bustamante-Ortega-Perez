bcc156bd8ecfde06c8c4a0a38688adb3f693e5a9
lorena perez <lorena879perez@gmail.com>
2023-05-03 16:00:01 -0300

    se agrego boton eliminar y se mojoro la estetica del boton seguir, imagen y demas cosas

--- script/script.js
+++ script/script.js
@@ -43,54 +43,69 @@ function cambiarTextoSeguir() {
  }
  
 function sumarLikes() {
-    // Obtener el elemento que muestra la cantidad de likes
     const likesCount = document.getElementById("numMegusta");
-  
-    // Obtener el botón "Me gusta"
     const botonMegusta = document.getElementById("boton-megusta");
-  
-    // Obtener la cantidad actual de likes
     let likes = parseInt(likesCount.innerText);
-  
-    // Incrementar la cantidad de likes en 1
     likes++;
-  
-    // Actualizar el contenido del elemento con la nueva cantidad de likes
     likesCount.innerText = likes + " ";
-  
-    // Deshabilitar el botón "Me gusta"
     botonMegusta.disabled = true;
-
     botonMegusta.innerText = "¡Gracias por tu like!";
   }
 
-
   function ingresar() {
     let usuario = document.getElementById("usuario").value;
-  
-    if (usuario === "") {
-      alert("No ha ingresado un nombre")
+    let mensajeUsuario = document.getElementById("mensaje-usuario");
+
+   
+    if (usuario === ""  && usuario === null) {
+      mensajeUsuario.innerHTML = "No ha ingresado un usuario";
+      mensajeUsuario.style.display = "block";
       return;
-    } else {
+  } else {
       localStorage.setItem("usuario", usuario);
-      
-    }
+      mensajeUsuario.style.display = "none";  
   }
+  }
+
+
   function comentar() {
+    let usuario = localStorage.getItem("usuario");
+    let mensajeComentario = document.getElementById("mensaje-comentario");
+  
+    if (!usuario) {
+      mensajeUsuario.innerHTML = "No ha ingresado un usuario";
+      return;
+    }
+  
     let comentario = document.getElementById("comentario-texto").value;
   
     if (comentario === "") {
-      alert("No ha ingresado un comentario")
+      mensajeComentario.innerHTML = "No ha ingresado un comentario";
+      mensajeComentario.style.display = "block";
       return;
     } else {
       let usuario = localStorage.getItem("usuario");
       let seccionComentarios = document.getElementById("seccion-comentarios");
       let nuevoComentario = document.createElement("div");
+
       nuevoComentario.classList.add("comentario");
       nuevoComentario.innerHTML = "<span class='nombre-usuario' id='nuevo-usuario'>" + usuario + "</span>" + " " + "<span class='texto-comentario'>" + comentario + "</span>";
+      const botonBorrar = document.createElement('button');
+      botonBorrar.classList.add('botonBorrar');
+      botonBorrar.innerText = "Eliminar";
+      
+      nuevoComentario.appendChild(botonBorrar);
       seccionComentarios.appendChild(nuevoComentario);
       document.getElementById("comentario-texto").value = "";
+      mensajeComentario.style.display = "none";
+  
+      botonBorrar.addEventListener('click', () => {
+        seccionComentarios.removeChild(nuevoComentario);
+      });
     }
   }
+  
+
+
 
 
